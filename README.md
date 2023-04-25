b1b67d16a1c3644add2437c6089adfbdfd9dc092
Kyra Polk <Kyraambeau@gmail.com>
2023-04-25 02:09:23 +0000

    description

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # kyra-polk.github.io
+
+# How to install new projects
+1)In terminal type => c
+2)On line 4 of .bashrc file, type => export CHE_PROJECTS_ROOT=/workspace
+3)Exit out of .bashrc file
+4) kill terminal
+5)reopen terminal
+6) In terminal, type => sudo apt-get install subversion
+7) In terminal, type => npm install -g opspark
+8) In terminal, type => os install
+
+# first website project
+- Complete todos 1-10
+
+# Portfolio Project
+- Complete todos 1-5
+
+# How to test projects
+1)In the terminal type => os test
+2)In terminal type github user name if prompted 
+3)type personal access token if prompted
+4)select bootcamp session
+5)select project to test
+
+# How to run git commands 
+1)In terminal type => git add -A
+2) In terminal, type => git commit -m "describe your work"
+3) In terminal type => git push
---
+++ projects/scratch-pad/day-1/stringy.js
@@ -0,0 +1,235 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: stringy.js
+ */
+
+/**
+ * Given an input String, return its length.
+ *
+ * TIP: In JavaScript, how can we decipher the length of a String?
+ * work?
+ */
+function length(string) {
+    // YOUR CODE BELOW HERE //
+    //return the length of the string using .length method
+    return string.length;
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to lowercase.
+ */
+function toLowerCase(string) {
+    // YOUR CODE BELOW HERE //
+//return the input as a new string by using toLowercase
+ return string.toLowerCase();
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to uppercase.
+ */
+function toUpperCase(string) {
+    // YOUR CODE BELOW HERE //
+//return a new string using .toUpperCase
+return string.toUpperCase();
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to dash-case.
+ *
+ * Examples:
+ *
+ *      toDashCase('Hello World'); // => 'hello-world'
+ *
+ * TIP: How can you look for and replace a space in a String? How can you
+ *      enforce lowercase? Look at the global replace link below, or, try String
+ *      methods split and join?
+ *
+ *      See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Using_global_and_ignore_with_replace()
+ */
+function toDashCase(string) {
+    // YOUR CODE BELOW HERE //
+    //need to ensure always gonna be lowercased string
+    
+/*I-any string, O-string dashcased,C- lowercase always, E-empty string?
+need to replace all spaces in the given string to dashes*/
+return string.toLowerCase().replaceAll(" ", "-");
+
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * begins with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      beginsWith('Max', 'm'); // => true;
+ *      beginsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function beginsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    //to make the function non discriminatory i can force both inputs to be lowercased
+    
+    
+//whatever the input string, the first letter must loosely equal the input for char
+//need an if statement
+if (string[0].toLowerCase() === char.toLowerCase()){
+    return true;
+} else {
+//if the first letter of the string is not the same letter as char, the function should return false
+    return false;
+}
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * ends with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      endsWith('Max', 'X'); // => true;
+ *      endsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function endsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+    //need to make it so that regardless of capitalization or not, the last letter and char will always be lowercased for comparison
+    
+//if the last letter in the string is the same letter as char, the function should return true
+if (string[string.length - 1].toLowerCase() === char.toLowerCase()){
+    return true;
+    //if the last letter of the string is not the same letter as char, then the function should return false 
+} else {
+    return false;
+}
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two input Strings, return the Strings concatenated into one.
+ *
+ * TIP: What's the operator to concatenate two Strings?
+ */
+function concat(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+//concatenate the two strings using .concat
+return stringOne.concat(stringTwo);
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given any number of Strings, return all of them joined together.
+ *
+ * Example:
+ *
+ *      join("my", "name", "is", "Ben"); // => "mynameisBen";
+ *
+ * TIP: This Function pulls out all the arguments passed to it and stores them
+ *      in an Array called args.
+ */
+function join(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    var args = Array.from(arguments);
+//using .join i need to join together all strings passed in as an arguement and return the result
+return args.join("");
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return the longest of the two.
+ *
+ * Example:
+ *
+ *      longest("ben", "maggie");   //-> "maggie"
+ *
+ * TIP: What property of the String do we need to compare?
+ */
+function longest(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+//if string one .length is longer than string two .length return string 1
+//create an if statement comparing the lengths
+if (stringOne.length > stringTwo.length){
+    return stringOne;
+    //if string one is less than string two return string two
+} else if (stringOne.length < stringTwo.length){
+    return stringTwo;
+}
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return 1 if the first is higher in alphabetical order than
+ * the second, return -1 if the second is higher in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortAscending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+//If string one is higher in alphabetical order than string two,return 1
+if (stringOne[0])
+//If string two is higher in order than string one, return -1
+//if they are equal return 0
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+/**
+ * Given two Strings, return 1 if the first is lower in alphabetical order than
+ * the second, return -1 if the second is lower in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortDescending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.length = length;
+    module.exports.toLowerCase = toLowerCase;
+    module.exports.toUpperCase = toUpperCase;
+    module.exports.toDashCase = toDashCase;
+    module.exports.beginsWith = beginsWith;
+    module.exports.endsWith = endsWith;
+    module.exports.concat = concat;
+    module.exports.join = join;
+    module.exports.longest = longest;
+    module.exports.sortAscending = sortAscending
+    module.exports.sortDescending = sortDescending;
+}
